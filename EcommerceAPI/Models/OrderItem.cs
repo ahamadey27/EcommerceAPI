8271ab7c0598a9e89c01088d4e5e2eb734c23f37
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-06-22 21:45:37 -0400

    finished with model scripts

--- EcommerceAPI/Models/OrderItem.cs
+++ EcommerceAPI/Models/OrderItem.cs
@@ -21,6 +21,10 @@ namespace EcommerceAPI.Models
         public int ProductId { get; set; }
         public Product Product { get; set; } = null!;
 
+        //Link to parent order
+        public int OrderId { get; set; }
+        public Order Order { get; set; } = null!;
+
 
 
     }
