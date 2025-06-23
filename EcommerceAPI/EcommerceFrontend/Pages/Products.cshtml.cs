179ad19c69cbe2d152f192c4ab2fffe5fafb1fdf
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-06-23 15:58:57 -0400

    Add Cart, Products, and Success pages with functionality for displaying products, managing cart, and order confirmation

---
+++ EcommerceAPI/EcommerceFrontend/Pages/Products.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EcommerceFrontend.Pages
+{
+    public class ProductsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
