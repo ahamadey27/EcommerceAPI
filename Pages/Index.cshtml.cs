f16ddb8d91a097fb078bfa2f39041a8a4e95ccef
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-06-24 12:13:50 -0400

    final push - added delete button too

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace EcommerceFrontend.Pages;
+namespace EcommerceAPI.Pages;
 
 public class IndexModel : PageModel
 {
